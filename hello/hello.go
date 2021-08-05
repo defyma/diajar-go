7f46c758fda7aacccadae0b68a3e2e28f2229199
Defy M Aminuddin <defyma85@gmail.com>
2021-08-05 20:15:09 +0700

    quote

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,12 @@
 package main
 
 import "fmt"
-
+import "rsc.io/quote"
 func main() {
-	fmt.Println("Hello, World!")
+	/*
+		Command: "go mod tidy"
+		Go will add the quote module as a requirement, as well as a go.sum file for use in authenticating the module.
+	*/
+
+	fmt.Println(quote.Go())
 }
